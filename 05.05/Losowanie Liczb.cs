2a4895c4fc6a4acbdf903beb786d7762a0c9586a
Oskar Kubala <90306268+Texas02@users.noreply.github.com>
2024-09-05 10:02:24 +0200

    Add files via upload

---
+++ 05.05/Losowanie Liczb.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tablica;
+            Random rand = new Random();
+            tablica = new int[200];
+
+            for (int i=0; i<tablica.Length; i++)
+            {
+                int random = rand.Next(1000);
+                tablica[i] = random;
+                Console.Write(tablica[i] + " ");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
